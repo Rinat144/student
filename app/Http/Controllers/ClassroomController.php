9f1d2344959ee1820492b33c4bcff309911d62a3
Rinat144 <rinatik14@gmail.com>
2022-08-14 20:39:12 +0300

    classroomcont new update

--- app/Http/Controllers/ClassroomController.php
+++ app/Http/Controllers/ClassroomController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Classroom\CreateRequests;
 use App\Models\Classroom;
 use App\Models\Student;
 use Illuminate\Http\Request;
@@ -12,7 +13,7 @@ class ClassroomController extends Controller
     {
         $classrooms = Classroom::pluck('name');
         return response()->json([
-           'classrooms' => $classrooms,
+            'classrooms' => $classrooms,
         ]);
     }
 
@@ -27,11 +28,44 @@ class ClassroomController extends Controller
 
     public function curriculum(Classroom $classroom)
     {
-      $lectures = Classroom::leftJoin('classroom_lectures', 'classrooms.id', '=', 'classroom_lectures.classroom_id')
-      ->leftJoin('lectures', 'classroom_lectures.lecture_id', '=', 'lectures.id')
-      ->where('classrooms.id', '=', $classroom->id)
-          ->pluck('lectures.topic');
+        $lectures = Classroom::leftJoin('classroom_lectures', 'classrooms.id', '=', 'classroom_lectures.classroom_id')
+            ->leftJoin('lectures', 'classroom_lectures.lecture_id', '=', 'lectures.id')
+            ->where('classrooms.id', '=', $classroom->id)
+            ->pluck('lectures.topic');
 
-      dd($lectures);
+        return response()->json([
+            'list of lectures' => $lectures,
+        ]);
+    }
+
+    public function createClassroom(CreateRequests $requests)
+    {
+        $data = $requests->validated();
+        Classroom::create([
+            'name' => $data['name']
+        ]);
+
+        return response()->json([
+            'status' => true,
+        ]);
+    }
+
+    public function update(CreateRequests $requests, Classroom $classroom)
+    {
+        $data = $requests->validated();
+        $classroom->update($data);
+
+        return response()->json([
+            'status' => true,
+        ]);
+    }
+
+    public function delete($id)
+    {
+        Classroom::destroy($id);
+
+        return response()->json([
+           'status' => true,
+        ]);
     }
 }
